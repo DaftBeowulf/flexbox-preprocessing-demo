26b2fb3749c00a04271871a10ecf5a1842f8ac71
DaftBeowulf <ajmboosh@gmail.com>
2019-01-24 15:16:18 -0600

    changed folder name, added JS-fundamentals portion

---
+++ JS-fundamentals/array-methods.js
@@ -0,0 +1,38 @@
+let smashSeries = [
+  {
+    name: "Legend of Zelda",
+    games: 19,
+    protagonist: "Link",
+    antagonist: "Ganon",
+    location: "Hyrule"
+  },
+  {
+    name: "Mario",
+    games: 200,
+    protagonist: "Mario",
+    antagonist: "Bowser",
+    location: "Mushroom Kingdom"
+  },
+  {
+    name: "Metroid",
+    games: 14,
+    protagonist: "Samus Aran",
+    antagonist: "Ridley",
+    location: "space"
+  },
+  {
+    name: "F-Zero",
+    games: 9,
+    protagonist: "Captain Falcon",
+    antagonist: "Black Shadow",
+    location: "racetrack"
+  }
+];
+
+// forEach
+
+// map
+
+// filter
+
+// reduce
